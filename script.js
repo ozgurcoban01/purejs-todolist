c89c25f2e908bae397d0a65c0ca612fe753f3938
ozgurcoban01 <cobanozgur55@gmail.com>
2023-07-21 14:54:48 +0300

    almoest ready for css

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const todoAddButton = document.querySelector(".added-confirm");
 const waitingTodos = document.querySelector(".todos");
 const finishedTodos = document.querySelector(".finished-todos");
 
+let newTodos;
+let newTodosFinishedButton;
+let newTodosDeleteButton;
+
 todoAddButton.addEventListener("click", newTodoAdd);
 
 let todos = undefined;
@@ -21,15 +25,15 @@ if (localStorage.getItem("todosId") == 0) {
 function orderTodos() {
   waitingTodos.innerHTML = "";
   finishedTodos.innerHTML = "";
+
   todos.forEach((todo) => {
     if (todo.finished == true) {
-      
       const newTodo = document.createElement("div");
       newTodo.classList.add("finished-todo");
 
-      const newTodoContent = document.createElement("div");
+      const newTodoContent = document.createElement("input");
       newTodoContent.classList.add("finished-todo-content");
-      newTodoContent.innerText = todo.todo;
+      newTodoContent.value = todo.todo;
 
       const newTodoAdjust = document.createElement("div");
       newTodoAdjust.classList.add("finished-todo-adjust");
@@ -45,13 +49,12 @@ function orderTodos() {
 
       finishedTodos.appendChild(newTodo);
     } else {
-
       const newTodo = document.createElement("div");
       newTodo.classList.add("todo");
 
-      const newTodoContent = document.createElement("div");
+      const newTodoContent = document.createElement("input");
       newTodoContent.classList.add("todo-content");
-      newTodoContent.innerText = todo.todo;
+      newTodoContent.value = todo.todo;
 
       const newTodoAdjust = document.createElement("div");
       newTodoAdjust.classList.add("todo-adjust");
@@ -68,22 +71,45 @@ function orderTodos() {
       waitingTodos.appendChild(newTodo);
     }
   });
+
+  newTodos = document.querySelectorAll(".todo-content");
+  newTodosFinishedButton = document.querySelectorAll(".todo-adjust");
+  newTodosDeleteButton = document.querySelectorAll(".todo-delete");
+
+  newTodosFinishedButton.forEach((button) => {
+    button.addEventListener("click", () => {
+      orderAgain(button)
+    });
+  });
+  
+}
+
+function orderAgain(button) {
+  todos.forEach(todo=> {
+    if(todo.todoId==button.getAttribute("todoId")&&todo.finished==false){
+      todo.finished=true
+    }
+  });
+
+  orderTodos();
 }
 
 function newTodoAdd() {
-  localStorage.setItem(
-    "todosId",
-    parseInt(localStorage.getItem("todosId")) + 1
-  );
+  if (todoInput.value != "") {
+    localStorage.setItem(
+      "todosId",
+      parseInt(localStorage.getItem("todosId")) + 1
+    );
 
-  todos.unshift({
-    todo: todoInput.value,
-    finished: true,
-    todoId: localStorage.getItem("todosId"),
-  });
+    todos.unshift({
+      todo: todoInput.value,
+      finished: false,
+      todoId: localStorage.getItem("todosId"),
+    });
 
-  localStorage.setItem("todos", JSON.stringify(todos));
-  todoInput.value = "";
+    localStorage.setItem("todos", JSON.stringify(todos));
+    todoInput.value = "";
 
-  orderTodos();
+    orderTodos();
+  }
 }
